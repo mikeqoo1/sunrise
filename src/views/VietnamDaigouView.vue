f5058821ff00d2fdcfb7688a08ff5235da21e883
Mike Lin <mikeqoo123@gmail.com>
2025-12-06 22:12:37 +0800

    更新

--- src/views/VietnamDaigouView.vue
+++ src/views/VietnamDaigouView.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
 const items = [
+  { name: "北臉代購", price: "NT$ ???? / 件", image: "vietnam/TheNorthFace.png", note: "R庭*1 加菲*1 大熊*1 熊媽*1" },
+  { name: "第一順位 UE女款綠色DRYVENT防水衝鋒衣(8E6S0QP)", price: "R庭S號 NT$ ???? / 件", image: "vietnam/8E6S0QP.png", note: "8E6S0QP" },
+  { name: "第二順位 UE女款棕色DRYVENT防水衝鋒衣(8CHXEB0)", price: "R庭S號 NT$ ???? / 件", image: "vietnam/8CHXEB0.png", note: "8CHXEB0" },
+  { name: "第二順位 UE女款黑色DRYVENT防水衝鋒衣(8CHXJK3)", price: "R庭S號 NT$ ???? / 件", image: "vietnam/8CHXJK3.png", note: "8CHXJK3" },
+  { name: "經典1996男款黑色DWR防潑水羽絨外套(3C8DGOE)", price: "加菲S號 NT$ ???? / 件", image: "vietnam/3C8DGOE.jpg", note: "3C8DGOE" },
+  { name: "經典1996男款黑色DWR防潑水羽絨外套(3C8DGOE)", price: "熊媽M號 NT$ ???? / 件", image: "vietnam/3C8DGOE.jpg", note: "3C8DGOE" },
+  { name: "第一順位 M MOUNTAIN LIGHT TRICLIMATE GTX JACKET(NF0A83RRGOE)", price: "大熊3XL號 NT$ ???? / 件", image: "vietnam/NF0A83RRGOE.jpg", note: "NF0A83RRGOE" },
+  { name: "第二順位 M SANGRO DRYVENT JACKET 2.0 - AP(NF0A8DX1JK3)", price: "大熊3XL號 NT$ ???? / 件", image: "vietnam/NF0A8DX1JK3.jpg", note: "NF0A8DX1JK3" },
+  { name: "始祖鳥代購", price: "NT$ ???? / 件", image: "vietnam/ARCTERYX.png", note: "大熊*1" },
+  { name: "Arcteryx 始祖鳥 Atom Hoody 化纖保暖外套(X00000955602.jpg)", price: "大熊2XL號 NT$ ???? / 件", image: "vietnam/X00000955602.jpg", note: "X00000955602" },
   { name: "瑜伽褲", price: "NT$ ??? / 件", image: "vietnam/lululemon.png", note: "瑜伽系列" },
   { name: "運動服", price: "NT$ ??? / 件", image: "vietnam/nike.png", note: "運動系列" },
-  { name: "外套A", price: "NT$ ???? / 件", image: "vietnam/ARCTERYX.png", note: "冬天外套" },
-  { name: "外套B", price: "NT$ ???? / 件", image: "vietnam/TheNorthFace.png", note: "冬天外套" },
 ];
 </script>
 
